921f55164ebb71b6618aaa14201bc7029d0d3151
Arpit Goyal <agmps17@gmail.com>
2014-06-26 09:29:54 +0530

    making routes compatible with rails-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ DisksInfo::Engine.routes.draw do
 	# root of the plugin
         root :to => 'disks_info#index'
 	# examples of controllers built in this generator. delete at will
-	match 'mounts' => 'disks_info#mounts'
+	match 'mounts' => 'disks_info#mounts',:via=>:get
 end
